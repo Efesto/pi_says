76e2abbd4d2dbc504a059c6ed3bd3524162173e5
Efesto <polita.marco@gmail.com>
2020-07-05 12:17:35 +0200

    child_spec for worker

--- lib/pi_says/worker.ex
+++ lib/pi_says/worker.ex
@@ -1,12 +1,22 @@
 defmodule PiSays.Worker do
   @game_board Application.fetch_env!(:pi_says, :game_board)
 
-  def start_link(arg) do
-    pid = spawn_link(__MODULE__, :play, arg)
+  def start_link(_arg) do
+    pid = spawn_link(__MODULE__, :play, [])
     {:ok, pid}
   end
 
-  def play(_arg) do
+  def child_spec(opts) do
+    %{
+      id: __MODULE__,
+      start: {__MODULE__, :start_link, [opts]},
+      type: :worker,
+      restart: :permanent,
+      shutdown: 500
+    }
+  end
+
+  def play() do
     @game_board.new
     |> PiSays.play()
   end
